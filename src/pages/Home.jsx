3695f6a6a0b18218089794b8ac91c4103fb0605a
Naveen Deemantha <117744019+NaveenDeemantha@users.noreply.github.com>
2025-09-20 12:00:34 +0530

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,17 @@ export const Home = () => {
   return (
     <div>
       <h1>Home Page Test</h1>
+
+      {/* {Theme Toggle} */}
+
+      {/* {Background Effects} */}
+
+      {/* { Navbar } */}
+
+      {/* { Main Content} */}
+
+      {/* { Footer } */}
+
     </div>
   );
 };
