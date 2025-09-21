99daa83b6d735aa8639cf2e575e9095220df4da5
Naveen Deemantha <117744019+NaveenDeemantha@users.noreply.github.com>
2025-09-21 17:30:15 +0530

    Star-background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Star } from "lucide-react";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background  text-foreground overflow-x-hidden">
 
       <div>Hello World</div>
+
       {/* {Theme Toggle} */}
       <ThemeToggle />
 
       {/* {Background Effects} */}
+      <StarBackground />
 
       {/* { Navbar } */}
 
