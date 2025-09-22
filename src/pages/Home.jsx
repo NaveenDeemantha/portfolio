dca1495b011070a776139be8984f4ca5a881ef5c
Naveen Deemantha <117744019+NaveenDeemantha@users.noreply.github.com>
2025-09-22 10:13:54 +0530

    test-changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ export const Home = () => {
   return (
     <div className="min-h-screen bg-background  text-foreground overflow-x-hidden">
 
+{/* <div>Hello World</div> */}
       {/* {Theme Toggle} */}
       <ThemeToggle />
 
